5453d86159883192085f20a9620bb8ca5485cd16
weixin <luweixinly@gmail.com>
2023-10-20 06:36:13 -0400

    polynomial fit and residuals

--- ps-5/signalanalysis.py
+++ ps-5/signalanalysis.py
@@ -19,20 +19,93 @@ with open('signal.dat', 'r') as f:
                 data.append(float(i)) 
 
 data = np.array(data, dtype='float')
-time_list = data[::2]
-signal_list = data[1::2]
+time_arr = data[::2]
+signal_arr = data[1::2]
+data_num = len(time_arr)
 
 def qa():
-    plt.scatter(time_list, signal_list, s=1)
+    plt.scatter(time_arr, signal_arr, s=1)
     plt.title("Signal Data")
     plt.xlabel("time")
     plt.ylabel("signal")
     plt.savefig("signal.png")
-    print(f"data length: {len(time_list)}")
+    plt.clf()
+    print(f"data length: {len(data_num)}")
 
 def qb():
     '''
-    fit a third-order polynomial
+    fit a third-order polynomial using SVD
     '''
+    # time_arr_T = time_arr.reshape(-1,1)
+    # signal_arr_T = signal_arr.reshape(-1,1)
+    T = np.ones((data_num, 4))
+    T[:,1] = time_arr
+    T[:,2] = pow(time_arr, 2)
+    T[:,3] = pow(time_arr, 3)
+    # print(T[:2])
+    Y = signal_arr
+    (u, w, vt) = np.linalg.svd(T, full_matrices=False)
+    T_inv = vt.transpose().dot(np.diag(1. / w)).dot(u.transpose())
+    A = T_inv.dot(Y)
+    print(f"third-order polynomial fit: {A[0]} + {A[1]}t + {A[2]}t^2 + {A[3]}t^3")
+    ys = T.dot(A)
+    plt.scatter(time_arr, ys, s=1, label = "third-order polynomial fit")
+    plt.scatter(time_arr, signal_arr, s=1, label = "original signal")
+    plt.title("Third-order Polynomial Fit")
+    plt.legend()
+    plt.xlabel("time")
+    plt.ylabel("signal")
+    plt.savefig("third-order-poly.png")
+    plt.clf()
+
+def qb_time_scaled():
+    '''
+    fit a third-order polynomial using SVD
+    '''
+    time_arr_scaled = time_arr / 1e9
+    T = np.ones((data_num, 4))
+    T[:,1] = time_arr_scaled
+    T[:,2] = pow(time_arr_scaled, 2)
+    T[:,3] = pow(time_arr_scaled, 3)
+    # print(T[:2])
+    Y = signal_arr
+    (u, w, vt) = np.linalg.svd(T, full_matrices=False)
+    T_inv = vt.transpose().dot(np.diag(1. / w)).dot(u.transpose())
+    A = T_inv.dot(Y)
+    print(f"third-order polynomial fit (time scaled by 1e-9): {A[0]} + {A[1]}t + {A[2]}t^2 + {A[3]}t^3")
+    ys = T.dot(A)
+    plt.scatter(time_arr_scaled, ys, s=1, label = "third-order polynomial fit")
+    plt.scatter(time_arr_scaled, signal_arr, s=1, label = "original signal")
+    plt.title("Third-order Polynomial Fit (time scaled by 1e-9)")
+    plt.legend()
+    plt.xlabel("time (1e9*unit)")
+    plt.ylabel("signal")
+    plt.savefig("third-order-poly-time-scaled.png")
+    plt.clf()
+
+def qc():
+    '''
+    residual
+    '''
+    time_arr_scaled = time_arr / 1e9
+    T = np.ones((data_num, 4))
+    T[:,1] = time_arr_scaled
+    T[:,2] = pow(time_arr_scaled, 2)
+    T[:,3] = pow(time_arr_scaled, 3)
+    Y = signal_arr
+    (u, w, vt) = np.linalg.svd(T, full_matrices=False)
+    T_inv = vt.transpose().dot(np.diag(1. / w)).dot(u.transpose())
+    A = T_inv.dot(Y)
+    ys = T.dot(A)
+    residuals = Y - ys
+    plt.scatter(time_arr_scaled, residuals, s = 1)
+    plt.title("Residuals after Third-order Polynomial Fit")
+    plt.xlabel("time (1e9*unit)")
+    plt.ylabel("signal")
+    plt.savefig("residual-third-order-poly.png")
+    
 
 # qa()
+# qb()
+# qb_time_scaled()
+qc()
